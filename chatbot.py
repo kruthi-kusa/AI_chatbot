d05f77e0d2b020dc93caa4df81cb37fd4d353bb1
Kruthi <kruthireddy8888@gmail.com>
2025-02-12 22:03:13 -0800

    reverted to original youtube channel change

--- chatbot.py
+++ chatbot.py
@@ -9,23 +9,24 @@ from langchain_huggingface import HuggingFaceEndpoint
 import chainlit as cl
 
 
-# HUGGINFACEHUB_API_TOKEN=getpass()
-# os.environ['HUGGINFACEHUB_API_TOKEN']=HUGGINFACEHUB_API_TOKEN
+#HUGGINFACEHUB_API_TOKEN=getpass()
+#os.environ['HUGGINFACEHUB_API_TOKEN']=HUGGINFACEHUB_API_TOKEN
+
+os.environ['HUGGINGFACEHUB_API_TOKEN'] = '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'  
 
 load_dotenv()
 
 model_id = "gpt2-medium"
 conv_model = HuggingFaceEndpoint(
-    huggingfacehub_api_token=os.environ['HUGGINGFACEHUB_API_TOKEN'
-                                        ],  # Note this parameter name
+    huggingfacehub_api_token=os.environ['HUGGINGFACEHUB_API_TOKEN'],  
     repo_id=model_id,
     temperature=0.9,
-    max_new_tokens=100
+    max_new_tokens=400
 )
 
-template= """"You are a helpful AI assistant. Please respond to the following message in a clear and engaging way {query}
+template= """You are a helpful AI assistant that makes stories  by completing the query provided by the user
+{query}
 """
-#message_content = message.content if hasattr(message, 'content') else str(message)
 
 @cl.on_chat_start
 def main():
@@ -40,16 +41,5 @@ def main():
 async def main(message:str):
     llm_chain = cl.user_session.get("llm_chain")
     res = await llm_chain.acall(message, callbacks = [cl.AsyncLangchainCallbackHandler()])
-    print(res)
 
-    
-    if res and "text" in res:
-        await cl.Message(content=res["text"]).send()
-    else:
-        await cl.Message(content="I apologize, but I couldn't generate a response. Please try again.").send()
-            
-
-    #await cl.Message(content=res["text"]).send()
-
-#print(conv_chain.run({"query": "Hello, all"}))
-#print(response)
+    await cl.Message(content=res["text"]).send()
